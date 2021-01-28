93aaf0cbfe7b17e956a67bf0f7af015379210e6e
paxasaval <paxasaval1003@gmail.com>
2021-01-28 11:21:32 -0500

    fix-resize-nav-technology

--- src/app/@components/header/header.component.ts
+++ src/app/@components/header/header.component.ts
@@ -7,33 +7,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   toHome() {
+    let toogle = document.getElementById("toogle");
+    if (toogle.classList.contains("toogle-active-sm")) {
+      let clases = document.getElementsByClassName("item")
+      document.getElementById("navbar").classList.add("fixed")
+      for (let i = 0; i < clases.length; i++) {
+        clases.item(i).classList.remove("item-active-sm")
+      }
+      toogle.classList.remove("toogle-active-sm")
+    }
     document.getElementById("home").scrollIntoView({ behavior: "smooth" });
   }
-  toAbout() {
+  toAbout() { 
+    let toogle = document.getElementById("toogle");
+    if (toogle.classList.contains("toogle-active-sm")) {
+      let clases = document.getElementsByClassName("item")
+      document.getElementById("navbar").classList.add("fixed")
+      for (let i = 0; i < clases.length; i++) {
+        clases.item(i).classList.remove("item-active-sm")
+      }
+      toogle.classList.remove("toogle-active-sm")
+    }
     document.getElementById("about").scrollIntoView({ behavior: "smooth" });
   }
   toProducts() {
+    let toogle = document.getElementById("toogle");
+    if (toogle.classList.contains("toogle-active-sm")) {
+      let clases = document.getElementsByClassName("item")
+      document.getElementById("navbar").classList.add("fixed")
+      for (let i = 0; i < clases.length; i++) {
+        clases.item(i).classList.remove("item-active-sm")
+      }
+      toogle.classList.remove("toogle-active-sm")
+    }
     document.getElementById("products").scrollIntoView({ behavior: "smooth" });
   }
   toTechnology() {
+    let toogle = document.getElementById("toogle");
+    if(toogle.classList.contains("toogle-active-sm")){
+      let clases = document.getElementsByClassName("item")
+      document.getElementById("navbar").classList.add("fixed")
+      for (let i = 0; i < clases.length; i++) {
+        clases.item(i).classList.remove("item-active-sm")
+      }
+      toogle.classList.remove("toogle-active-sm")
+    }
     document.getElementById("technology").scrollIntoView({ behavior: "smooth" });
   }
-  toToogle(){
-    
-    let toogle=document.getElementById("toogle")
-    console.log(toogle.classList)
-    
+  toToogle(){   
+    let toogle = document.getElementById("toogle");
     if (!toogle.classList.contains("toogle-active-sm")){
       let n = document.getElementsByClassName("item").length
       let clases = document.getElementsByClassName("item")
-      console.log(document.getElementById("navbar"))
-      document.getElementById("navbar").classList.remove("fixed")
+      //document.getElementById("navbar").classList.remove("fixed")
       for (let i=0; i<n; i++){
         clases.item(i).classList.add("item-active-sm")
       } 
@@ -47,6 +81,5 @@ export class HeaderComponent implements OnInit {
       }
       toogle.classList.remove("toogle-active-sm")
     }
-    document.getElementById("navbar").scrollIntoView({ behavior: "smooth" });
   }
 }
